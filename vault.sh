3358101e64918ae4ed94eea1c75efee968b36c4d
Brian McCafferty <brian@bdm.scot>
2019-11-05 19:41:40 +0000

    Added inital commit of the config settings file

--- vault.sh
+++ vault.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 #Cyberark API Bash Script
 #Brian McCafferty - 2019
+source config/vault.cfg
+echo "The following settings are being pulled from the config/vault.cfg settings file"
+echo "Vault URL: "$VAULT_URL
+echo "Vault Authentication Method: "$VAULT_AUTH
+echo "CyberArk Logon User: "$VAULT_USER
